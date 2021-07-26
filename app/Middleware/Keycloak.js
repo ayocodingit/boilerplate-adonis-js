f80e36711202cea5e0bdcfff715127c68da7a7cf
ayocodingit <ayocodingit@gmail.com>
2021-07-26 15:16:18 +0700

    feature keycloak

---
+++ app/Middleware/Keycloak.js
@@ -0,0 +1,41 @@
+'use strict'
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+
+const Config = use('Config')
+const jwt = require('jsonwebtoken')
+
+class Keycloak {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+  async handle ({ request, response, session }, next) {
+
+    const keycloakPem = Config.get('keycloak.keycloakPem')
+    const jwtConfig = Config.get('keycloak.config')
+
+    try {
+        const decoded = jwt.verify(this.getToken(request), keycloakPem, jwtConfig)
+        session.put('user', decoded)
+        await next()
+    } catch(err) {
+        session.forget('user')
+        return response.status(401).json({error: err.message})
+    }
+  }
+
+  getToken(request) {
+    const authorization = request.header('authorization')?.split(' ')
+
+    let token = null
+    if (authorization && authorization.length == 2) {
+      token = authorization[1]
+    }
+
+    return token
+  }
+}
+
+module.exports = Keycloak
