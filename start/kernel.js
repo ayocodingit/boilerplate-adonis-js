b63057b9065cf3e15145986b81dd38dfc6065b01
ayocodingit <ayocodingit@gmail.com>
2021-08-09 22:01:02 +0700

    remove keycloak

--- start/kernel.js
+++ start/kernel.js
@@ -39,8 +39,7 @@ const globalMiddleware = [
 */
 const namedMiddleware = {
   auth: 'Adonis/Middleware/Auth',
-  guest: 'Adonis/Middleware/AllowGuestOnly',
-  keycloak: 'App/Middleware/Keycloak'
+  guest: 'Adonis/Middleware/AllowGuestOnly'
 }
 
 /*
