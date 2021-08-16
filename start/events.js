be46f2e22c7532428fa9b3340479e5467e3d5e74
ayocodingit <ayocodingit@gmail.com>
2021-08-16 16:28:31 +0700

    changes directory monorepo src

--- start/events.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-/** @type {typeof import('@adonisjs/framework/src/Event')} */
-// const Event = use('Event')
-
-/** @type {typeof import('adonisjs-queue/src/Queue')} */
-// const Queue = use('Queue')
