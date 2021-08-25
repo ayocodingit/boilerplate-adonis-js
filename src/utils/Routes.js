2eb1f251439e6f438ced391da1a533e897607bb8
ayocodingit <ayocodingit@gmail.com>
2021-08-25 22:14:01 +0700

    update enum utils

--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -2,8 +2,10 @@
 
 const Route = use('Route')
 
-module.exports = (path, controller, validator) => {
+const Resource = (path, controller, validator) => {
   return Route.resource(path, controller)
     .apiOnly()
     .validator(new Map(validator))
 }
+
+module.exports = { Resource }
