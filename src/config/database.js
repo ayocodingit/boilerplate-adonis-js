12680e1063531443dde7f091e2fd965581283dd1
ayocodingit <ayocodingit@gmail.com>
2021-08-27 16:42:07 +0700

    add costum pooling

--- src/config/database.js
+++ src/config/database.js
@@ -57,7 +57,8 @@ module.exports = {
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis')
     },
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get('DB_DEBUG', false),
+    pool: { min: 0, max: 7 }
   },
 
   /*
@@ -79,6 +80,7 @@ module.exports = {
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis')
     },
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get('DB_DEBUG', false),
+    pool: { min: 0, max: 7 }
   }
 }
