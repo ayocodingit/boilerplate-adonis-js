e4aeea6cabb269202456e6ef3cec5219f875b127
ayocodingit <ayocodingit@gmail.com>
2021-08-29 18:57:50 +0700

    validation format

--- src/utils/Validators.js
+++ src/utils/Validators.js
@@ -5,9 +5,20 @@ const { StatusCodes } = require('http-status-codes')
 const failResponse = (ctx, errorMessages) => {
   return ctx.response
     .status(StatusCodes.UNPROCESSABLE_ENTITY)
-    .json({ errors: errorMessages })
+    .json({ errors: formatMessage(errorMessages) })
+}
+
+const formatMessage = (errorMessages) => {
+  const errors = {}
+
+  for (const error of errorMessages) {
+    errors[error.field] = [error.message]
+  }
+
+  return errors
 }
 
 module.exports = {
-  failResponse
+  failResponse,
+  formatMessage
 }
