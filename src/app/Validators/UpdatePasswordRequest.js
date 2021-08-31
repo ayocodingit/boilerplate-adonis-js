38952de930e5c5b2f2971dc1d4826e7b589f5de0
ayocodingit <ayocodingit@gmail.com>
2021-08-31 22:05:56 +0700

    update password old

--- src/app/Validators/UpdatePasswordRequest.js
+++ src/app/Validators/UpdatePasswordRequest.js
@@ -2,6 +2,8 @@
 
 const { formatMessage } = use('Antl')
 const { failResponse } = use('utils/Validators')
+const User = use('App/Models/User')
+const Hash = use('Hash')
 
 class UpdatePasswordRequest {
   get validateAll () {
@@ -10,14 +12,31 @@ class UpdatePasswordRequest {
 
   get rules () {
     return {
-      password: 'required|confirmed'
+      password_valid: 'equals:true',
+      password_old: 'required',
+      password: 'required|min:6|confirmed'
     }
+  }
+
+  get data () {
+    const requestBody = this.ctx.request.all()
+    const auth = await this.ctx.auth.getUser()
+    const user = await User.query().setVisible(['password']).where('id', auth.id).first()
+    const password = user.password
+
+    const passwordValid = { password_valid: 'false'}
+    if (await Hash.verify(this.ctx.request.input('password'), password)) {
+      passwordValid.password_valid = 'true'
+    }
+    return Object.assign({}, requestBody, passwordValid)
   }
 
   get messages () {
     return {
+      'password_valid.equals': formatMessage('validation.same', { attribute: 'password_old', other: 'password sebelumnya' }),
       'password.required': formatMessage('validation.required', { attribute: 'password' }),
-      'password.confirmed': formatMessage('validation.confirmed', { attribute: 'password' })
+      'password.min': formatMessage('validation.min_numeric', { attribute: 'password', min: 6 }),
+      'password.confirmed': formatMessage('validation.confirmed', { attribute: 'password' }),
     }
   }
 
