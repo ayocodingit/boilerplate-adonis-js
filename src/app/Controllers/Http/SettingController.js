0179c00eb55fdab475651b2100ad98369063b551
ayocodingit <ayocodingit@gmail.com>
2021-09-01 21:30:50 +0700

    update password

--- src/app/Controllers/Http/SettingController.js
+++ src/app/Controllers/Http/SettingController.js
@@ -1,11 +1,23 @@
 'use strict'
 
+const User = use('App/Models/User')
+const Hash = use('Hash')
+const CustomException = use('App/Exceptions/CustomException')
+const { StatusCodes } = require('http-status-codes')
 class SettingController {
   async updatePassword ({ auth, request, response }) {
     const user = await auth.getUser()
+    await this.checkPasswordOld(user, request.input('password_old'))
     user.password = request.input('password')
     await user.save()
     return response.json({ message: 'password updated' })
+  }
+
+  async checkPasswordOld(user, password) {
+    const payload = await User.query().setVisible(['password']).where('id', user.uid).first()
+    if (await Hash.verify(password, payload.password)) {
+      throw new CustomException(formatMessage('auth.password_old_not_match'), StatusCodes.FORBIDDEN)
+    }
   }
 }
 
--- src/app/Validators/UpdatePasswordRequest.js
+++ src/app/Validators/UpdatePasswordRequest.js
@@ -2,8 +2,6 @@
 
 const { formatMessage } = use('Antl')
 const { failResponse } = use('utils/Validators')
-const User = use('App/Models/User')
-const Hash = use('Hash')
 
 class UpdatePasswordRequest {
   get validateAll () {
@@ -12,28 +10,14 @@ class UpdatePasswordRequest {
 
   get rules () {
     return {
-      password_valid: 'equals:true',
       password_old: 'required',
       password: 'required|min:6|confirmed'
     }
   }
 
-  async data () {
-    const requestBody = this.ctx.request.all()
-    const auth = await this.ctx.auth.getUser()
-    const user = await User.query().setVisible(['password']).where('id', auth.id).first()
-    const password = user.password
-
-    const passwordValid = { password_valid: 'false' }
-    if (await Hash.verify(this.ctx.request.input('password'), password)) {
-      passwordValid.password_valid = 'true'
-    }
-    return Object.assign({}, requestBody, passwordValid)
-  }
-
   get messages () {
     return {
-      'password_valid.equals': formatMessage('validation.same', { attribute: 'password_old', other: 'password sebelumnya' }),
+      'password_old.required': formatMessage('validation.required', { attribute: 'password_old' }),
       'password.required': formatMessage('validation.required', { attribute: 'password' }),
       'password.min': formatMessage('validation.min_numeric', { attribute: 'password', min: 6 }),
       'password.confirmed': formatMessage('validation.confirmed', { attribute: 'password' })
