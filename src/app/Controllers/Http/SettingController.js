570629f8fbadc03f4459a9812134d997a164a1d3
ayocodingit <ayocodingit@gmail.com>
2021-09-01 21:31:48 +0700

    update password lint

--- src/app/Controllers/Http/SettingController.js
+++ src/app/Controllers/Http/SettingController.js
@@ -4,6 +4,8 @@ const User = use('App/Models/User')
 const Hash = use('Hash')
 const CustomException = use('App/Exceptions/CustomException')
 const { StatusCodes } = require('http-status-codes')
+const { formatMessage } = use('Antl')
+
 class SettingController {
   async updatePassword ({ auth, request, response }) {
     const user = await auth.getUser()
@@ -13,7 +15,7 @@ class SettingController {
     return response.json({ message: 'password updated' })
   }
 
-  async checkPasswordOld(user, password) {
+  async checkPasswordOld (user, password) {
     const payload = await User.query().setVisible(['password']).where('id', user.uid).first()
     if (await Hash.verify(password, payload.password)) {
       throw new CustomException(formatMessage('auth.password_old_not_match'), StatusCodes.FORBIDDEN)
